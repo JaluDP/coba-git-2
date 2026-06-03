b2be04decee20c497b19032ca563c6837ae68e61
JaluDP <jdpscript@gmail.com>
2026-06-03 11:11:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat obcaecati odit ut officiis sapiente possimus! Deserunt illum aut, exercitationem soluta dolor facere nobis neque laboriosam explicabo. Nobis, omnis. A, iure.</p>
+    <ul>
+        <li>Satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
